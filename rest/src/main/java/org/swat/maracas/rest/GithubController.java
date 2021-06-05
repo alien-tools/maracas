39492b2d38160abe697bafbce4cd40a69fca1096
Thomas Degueule <thomas.degueule@labri.fr>
2021-06-05 17:21:42 +0200

    Basic error management

--- rest/src/main/java/org/swat/maracas/rest/GithubController.java
+++ rest/src/main/java/org/swat/maracas/rest/GithubController.java
@@ -13,6 +13,8 @@ import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
@@ -22,7 +24,10 @@ import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
 import org.kohsuke.github.GHCommitPointer;
+import org.kohsuke.github.GHFileNotFoundException;
 import org.kohsuke.github.GHPullRequest;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
@@ -30,10 +35,12 @@ import org.kohsuke.github.GitHubBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.swat.maracas.rest.data.BreakingChangeInstance;
 import org.swat.maracas.rest.data.PullRequestResponse;
 
@@ -44,22 +51,31 @@ import io.usethesource.vallang.IList;
 @RequestMapping("/github")
 public class GithubController {
 	private MaracasHelper maracas = MaracasHelper.getInstance();
+	private GitHub github;
 	private static final String CLONE_PATH = "./clones";
 	private static final Logger logger = LogManager.getLogger(GithubController.class);
 
 	@Autowired
     ResourceLoader resourceLoader;
 
-	// Considering the computation time, this should probably be a POST job/GET result duo
-	@GetMapping("/pr/{user}/{repository}/{pr}")
-	PullRequestResponse analyzePullRequest(@PathVariable String user, @PathVariable String repository, @PathVariable Integer pr) {
+	@PostConstruct
+	public void initialize() {
 		Resource githubRes = resourceLoader.getResource("classpath:.github");
 		try (InputStream in = githubRes.getInputStream()) {
 			Properties props = new Properties();
 			props.load(in);
+			this.github = GitHubBuilder.fromProperties(props).build();
+		} catch (IOException e) {
+			logger.error(e);
+		}
+	}
+
+	// Considering the computation time, this should probably be a POST job/GET result duo
+	@GetMapping("/pr/{user}/{repository}/{pr}")
+	PullRequestResponse analyzePullRequest(@PathVariable String user, @PathVariable String repository, @PathVariable Integer pr) {
+		try {
 			// Retrieve PR metadata from GH
-			GitHub gh = GitHubBuilder.fromProperties(props).build();
-			GHRepository repo = gh.getRepository(user + "/" + repository);
+			GHRepository repo = github.getRepository(String.format("%s/%s", user, repository));
 			GHPullRequest pullRequest = repo.getPullRequest(pr);
 			GHCommitPointer head = pullRequest.getHead();
 			GHCommitPointer base = pullRequest.getBase();
@@ -69,51 +85,59 @@ public class GithubController {
 			String baseUrl = base.getRepository().getHttpTransportUrl();
 			String headRef = head.getRef();
 			String baseRef = base.getRef();
-			
-			// Clone & build the BASE branch
 			Path basePath = Paths.get(CLONE_PATH).resolve(baseSha);
-			Optional<Path> v1 = cloneAndBuild(baseUrl, baseRef, basePath);
-
-			// Clone & build the HEAD branch
 			Path headPath = Paths.get(CLONE_PATH).resolve(headSha);
-			Optional<Path> v2 = cloneAndBuild(headUrl, headRef, headPath);
+
+			// Clone both branches
+			clone(baseUrl, baseRef, basePath);
+			clone(headUrl, headRef, headPath);
+
+			// Build both branches and retrieve the JARs
+			Path j1 = build(basePath);
+			Path j2 = build(headPath);
 			
-			if (v1.isPresent() && v2.isPresent()) {
-				Path j1 = v1.get();
-				Path j2 = v2.get();
-				
-				IList delta = maracas.computeDelta(j1, j2, basePath);
+			IList delta = maracas.computeDelta(j1, j2, basePath);
 
-				List<BreakingChangeInstance> bcs =
-					delta.stream()
-						.map(e -> BreakingChangeInstance.fromRascal((IConstructor) e))
-						.collect(Collectors.toList());
+			List<BreakingChangeInstance> bcs =
+				delta.stream()
+					.map(e -> BreakingChangeInstance.fromRascal((IConstructor) e))
+					.collect(Collectors.toList());
 
-				return new PullRequestResponse(headRef, baseRef, 0, bcs);
-			}
-		} catch (IOException | GitAPIException | MavenInvocationException e) {
+			return new PullRequestResponse(headRef, baseRef, 0, bcs);
+		} catch (GHFileNotFoundException e) {
 			logger.error(e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "The repository or PR does not exist", e);
+		} catch (GitAPIException e) {
+			logger.error(e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "git clone failed: " + e.getMessage(), e);
+		} catch (MavenInvocationException e) {
+			logger.error(e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Maven build failed: " + e.getMessage(), e);
+		} catch (IOException e) {
+			logger.error(e);
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown error: " + e.getMessage(), e);
 		}
-
-		return null;
 	}
 
-	private Optional<Path> cloneAndBuild(String url, String ref, Path path) throws MavenInvocationException, GitAPIException, IOException {
-		// FIXME
-		String fullRef = "refs/heads/" + ref;
-		if (!path.toFile().exists()) {
+	private void clone(String url, String ref, Path dest) throws InvalidRemoteException, TransportException, GitAPIException {
+		if (!dest.toFile().exists()) {
 			logger.info("Cloning {} [{}]", url, ref);
+			String fullRef = "refs/heads/" + ref; // FIXME
 			Git.cloneRepository()
 				.setURI(url)
 				.setBranchesToClone(Collections.singletonList(fullRef))
 				.setBranch(fullRef)
-				.setDirectory(path.toFile())
+				.setDirectory(dest.toFile())
 				.call();
-		}
-		
-		Path target = path.resolve("target");
+		} else logger.info("{} exists. Skipping.", dest);
+	}
+
+	private Path build(Path local) throws IOException, MavenInvocationException {
+		Path target = local.resolve("target");
+		Path pom = local.resolve("pom.xml");
 		if (!target.toFile().exists()) {
-			Path pom = path.resolve("pom.xml");
+			if (!pom.toFile().exists())
+				throw new MavenInvocationException("No root pom.xml file in " + local);
 
 			logger.info("Building {}", pom);
 			Properties properties = new Properties();
@@ -128,10 +152,15 @@ public class GithubController {
 		    Invoker invoker = new DefaultInvoker();
 		    invoker.setMavenHome(new File("/usr"));
 		    invoker.execute(request);
-		}
-		
+		} else logger.info("{} has already been built. Skipping.", local);
+
+		// FIXME: Just returning whatever .jar we found in /target/
 	    try (Stream<Path> walk = Files.walk(target, 1)) {
-	        return walk.filter(f -> f.toString().endsWith(".jar")).findFirst();
+	        Optional<Path> res = walk.filter(f -> f.toString().endsWith(".jar")).findFirst();
+	        if (res.isPresent())
+	        	return res.get();
+	        else
+	        	throw new MavenInvocationException("'package' goal on " + pom + " did not produce a JAR");
 	    }
 	}
 }
