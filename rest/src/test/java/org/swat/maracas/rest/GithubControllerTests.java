939c2e52b6dd8e899f4709a26ca4cfa962761a37
Thomas Degueule <thomas.degueule@labri.fr>
2021-06-09 23:34:10 +0200

    Some dumb tests for later

---
+++ rest/src/test/java/org/swat/maracas/rest/GithubControllerTests.java
@@ -0,0 +1,39 @@
+package org.swat.maracas.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class GithubControllerTests {
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	void testSubmitAndCheckPRSync() throws Exception {
+		mvc.perform(get("/github/pr-sync/tdegueul/comp-changes/2"))
+    		.andExpect(status().isOk())
+    		.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+    		.andExpect(jsonPath("$.message", is("ok")))
+    		.andExpect(jsonPath("$.delta.breakingChanges", not(empty())));
+	}
+
+	@Test
+	void testSubmitPRASync() throws Exception {
+		mvc.perform(post("/github/pr/tdegueul/comp-changes/2"))
+    		.andExpect(status().isAccepted())
+    		.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+    		.andExpect(result -> "/github/pr/tdegueul/comp-changes/2".equals(result.getResponse().getHeader("Location")))
+    		.andReturn();
+	}
+}
