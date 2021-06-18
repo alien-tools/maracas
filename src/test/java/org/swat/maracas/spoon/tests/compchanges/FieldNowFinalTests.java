ab494d6c142c851b0eecd1210c7ff44bece3a01d
Thomas Degueule <thomas.degueule@labri.fr>
2021-06-19 00:56:37 +0200

    FIELD_NOW_FINAL tests

---
+++ src/test/java/org/swat/maracas/spoon/tests/compchanges/FieldNowFinalTests.java
@@ -0,0 +1,43 @@
+package org.swat.maracas.spoon.tests.compchanges;
+
+import static japicmp.model.JApiCompatibilityChange.*;
+import static org.swat.maracas.spoon.Detection.APIUse.*;
+
+import org.junit.jupiter.api.Test;
+
+class FieldNowFinalTests extends CompChangesTest {
+	@Test
+	void testNoMore() {
+		assertNumberDetections(FIELD_NOW_FINAL, 6);
+	}
+
+	@Test
+	void testExtSuper() {
+		assertDetection("FieldNowFinalExt.java", 8, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+
+	@Test
+	void testExtNoSuper() {
+		assertDetection("FieldNowFinalExt.java", 17, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+	
+	@Test
+	void testExtSubSuper() {
+		assertDetection("FieldNowFinalExtSub.java", 8, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+
+	@Test
+	void testExtSupNoSuper() {
+		assertDetection("FieldNowFinalExtSub.java", 17, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+	
+	@Test
+	void testFA() {
+		assertDetection("FieldNowFinalFA.java", 10, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+
+	@Test
+	void testFASub() {
+		assertDetection("FieldNowFinalFA.java", 21, FIELD_NOW_FINAL, FIELD_ACCESS);
+	}
+}
