eafaf7b5fc676a7923baa0d1093aed6e099710ab
Thomas Degueule <thomas.degueule@labri.fr>
2021-06-22 11:00:00 +0200

    BreakBot: need to log when we're calling the callback

--- rest/src/main/java/org/swat/maracas/rest/breakbot/BreakBot.java
+++ rest/src/main/java/org/swat/maracas/rest/breakbot/BreakBot.java
@@ -4,16 +4,20 @@ import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.swat.maracas.rest.GithubService;
 import org.swat.maracas.rest.data.Delta;
 
 import net.minidev.json.JSONObject;
 
 public class BreakBot {
 	private final URI callbackUri;
+	private static final Logger logger = LogManager.getLogger(GithubService.class);
 
 	public BreakBot(URI callbackUri) {
 		this.callbackUri = callbackUri;
@@ -30,6 +34,7 @@ public class BreakBot {
 		HttpEntity<String> request = new HttpEntity<>(body, headers);
 		String res = rest.postForObject(callbackUri, request, String.class);
 
+		logger.info("Sent delta back to BreakBot ({}): {}", callbackUri, res);
 		return "ok".equals(res);
 	}
 }
