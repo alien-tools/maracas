1eab6097f9191898b79897d16d2b64ad52fe4ac7
Thomas Degueule <thomas.degueule@labri.fr>
2021-08-31 14:37:38 +0200

    MaracasReportTests

--- rest/src/test/java/org/swat/maracas/rest/MaracasReportTests.java
+++ rest/src/test/java/org/swat/maracas/rest/MaracasReportTests.java
@@ -1,21 +1,26 @@
 package org.swat.maracas.rest;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.not;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.swat.maracas.rest.data.Delta;
 import org.swat.maracas.rest.data.Detection;
 import org.swat.maracas.rest.data.MaracasReport;
 import org.swat.maracas.spoon.VersionAnalyzer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 class MaracasReportTests {
+	private MaracasReport report;
 
-	@Test
-	void test() throws Exception {
+	@BeforeEach
+	void setUp() {
 		Path v1 = Paths.get("/home/dig/repositories/comp-changes-data/old/target/comp-changes-0.0.1.jar");
 		Path v2 = Paths.get("/home/dig/repositories/comp-changes-data/new/target/comp-changes-0.0.2.jar");
 		Path c1 = Paths.get("/home/dig/repositories/comp-changes-data/client/src");
@@ -26,44 +31,51 @@ class MaracasReportTests {
 		analyzer.analyzeClient(c1);
 		analyzer.populateLocations(sources);
 
-		Delta delta = Delta.fromMaracasDelta(analyzer.getDelta());
-		ObjectMapper mapper = new ObjectMapper();
-    // pretty print
-    String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(delta);
-    System.out.println(json);
-
-		analyzer.getDetections().forEach(d -> {
-			try {
-				Detection detect = Detection.fromMaracasDetection(d);
-				String dJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(detect);
-				System.out.println(dJson);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-	}
-
-	@Test
-	void test2() throws Exception {
-		Path v1 = Paths.get("/home/dig/repositories/comp-changes-data/old/target/comp-changes-0.0.1.jar");
-		Path v2 = Paths.get("/home/dig/repositories/comp-changes-data/new/target/comp-changes-0.0.2.jar");
-		Path c1 = Paths.get("/home/dig/repositories/comp-changes-data/client/src");
-
-		VersionAnalyzer analyzer = new VersionAnalyzer(v1, v2);
-		analyzer.computeDelta();
-		analyzer.analyzeClient(c1);
-
-		MaracasReport report = new MaracasReport(
+		report = new MaracasReport(
 			Delta.fromMaracasDelta(analyzer.getDelta()),
 			analyzer.getDetections()
 				.stream()
 				.map(Detection::fromMaracasDetection)
 				.collect(Collectors.toSet())
 		);
+	}
 
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(report);
-		System.out.println(json);
+	@Test
+	void testSourceLocationsDelta() {
+		// Hamcrest's hasProperty doesn't work with records yet
+		//assertThat(
+		//	report.delta().brokenDeclarations(),
+		//	everyItem(allOf(
+		//		hasProperty("path", not(emptyString())),
+		//		hasProperty("startLine", not(equalTo(-1))),
+		//		hasProperty("endLine", not(equalTo(-1)))
+		//	))
+		//);
+
+		report.delta().brokenDeclarations().forEach(d -> {
+			assertThat(d.path(),      not(emptyString()));
+			assertThat(d.startLine(), greaterThan(0));
+			assertThat(d.startLine(), greaterThan(0));
+		});
+	}
+
+	@Test
+	void testSourceLocationsDetections() {
+		// Hamcrest's hasProperty doesn't work with records yet
+		//assertThat(
+		//	report.detections(),
+		//	everyItem(allOf(
+		//		hasProperty("path", not(emptyString())),
+		//		hasProperty("startLine", not(equalTo(-1))),
+		//		hasProperty("endLine", not(equalTo(-1)))
+		//	))
+		//);
+
+		report.detections().forEach(d -> {
+			assertThat(d.path(),      not(emptyString()));
+			assertThat(d.startLine(), greaterThan(0));
+			assertThat(d.startLine(), greaterThan(0));
+		});
 	}
 
 }
