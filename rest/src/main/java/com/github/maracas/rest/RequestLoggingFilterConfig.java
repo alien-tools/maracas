add198bbb59857186fd6e617d36f776f68435abc
Thomas Degueule <thomas.degueule@labri.fr>
2021-09-25 09:46:20 +0200

    rest: log all incoming requests

---
+++ rest/src/main/java/com/github/maracas/rest/RequestLoggingFilterConfig.java
@@ -0,0 +1,21 @@
+package com.github.maracas.rest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+          = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(true);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+}
