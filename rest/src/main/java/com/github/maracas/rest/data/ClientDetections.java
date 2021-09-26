3c5008c1259256bcebca0fea55635cf7030af40b
Thomas Degueule <thomas.degueule@labri.fr>
2021-09-26 13:20:33 +0200

    Forced to turn ClientDetections from Record to Class to make tests work

--- rest/src/main/java/com/github/maracas/rest/data/ClientDetections.java
+++ rest/src/main/java/com/github/maracas/rest/data/ClientDetections.java
@@ -1,18 +1,51 @@
 package com.github.maracas.rest.data;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public record ClientDetections(
-	String url,
-	List<Detection> detections,
-	Throwable error
-) {
-	public ClientDetections(String url, List<Detection> detections) {
-		this(url, detections, null);
+/**
+ * This is infuriating, but for some reason the record version of this datatype
+ * cannot be serialized _when running from tests_. No idea why. Back to the
+ * ugly class version...
+ */
+
+//public record ClientDetections(
+//	String url,
+//	List<Detection> detections,
+//	Throwable error
+//) {
+//	public ClientDetections(String url, List<Detection> detections) {
+//		this(url, detections, null);
+//	}
+//
+//	public ClientDetections(String url, Throwable error) {
+//		this(url, new ArrayList<>(), error);
+//	}
+//}
+
+public class ClientDetections {
+	private String url;
+	private List<Detection> detections;
+	private Throwable error;
+
+	public ClientDetections(String u, List<Detection> d, Throwable t) {
+		this.url = u;
+		this.detections = d;
+		this.error = t;
 	}
 
-	public ClientDetections(String url, Throwable error) {
-		this(url, new ArrayList<>(), error);
+	public ClientDetections(String u, List<Detection> d) {
+		this(u, d, null);
 	}
+
+	public ClientDetections(String u, Throwable t) {
+		this(u, null, t);
+	}
+
+	public String getUrl() { return url; }
+	public List<Detection> getDetections() { return detections; }
+	public Throwable getError() { return error; }
+
+	public void setUrl(String u) { url = u; }
+	public void setDetections(List<Detection> d) { detections = d; }
+	public void setError(Throwable t) { error = t; }
 }
