1dda105e32bd02b3a9ad33cea48bdc6c99e83769
Thomas Degueule <thomas.degueule@labri.fr>
2021-10-02 16:01:39 +0200

    Fix ClassNowAbstractTests

--- core/src/main/java/com/github/maracas/visitors/ClassNowAbstractVisitor.java
+++ core/src/main/java/com/github/maracas/visitors/ClassNowAbstractVisitor.java
@@ -9,6 +9,10 @@ import spoon.reflect.reference.CtTypeReference;
 /**
  * Detections of CLASS_NOW_ABSTRACT are:
  *	- Instantiations of the now-abstract class
+ *
+ * Note: In JApiCmp, types that go from {@code class} to {@code interface} are impacted
+ * by CLASS_NOW_ABSTRACT the same way types that go from {@code class} to
+ * {@code abstract class} are.
  */
 public class ClassNowAbstractVisitor extends BreakingChangeVisitor {
 	private final CtTypeReference<?> clsRef;
--- core/src/test/java/com/github/maracas/compchangestests/ClassNowAbstractTests.java
+++ core/src/test/java/com/github/maracas/compchangestests/ClassNowAbstractTests.java
@@ -6,20 +6,28 @@ import static japicmp.model.JApiCompatibilityChange.CLASS_NOW_ABSTRACT;
 import org.junit.jupiter.api.Test;
 
 class ClassNowAbstractTests extends CompChangesTest {
-	// TODO: japicmp reports CLASS_NOW_ABSTRACT for classes that go from Class to Interface
-	//       Weird behavior => fix upstream?
 	@Test
 	void testNoMore() {
-		assertNumberDetections(CLASS_NOW_ABSTRACT, 2);
+		assertNumberDetections(CLASS_NOW_ABSTRACT, 4);
 	}
 
 	@Test
-	void testCreateObject() {
+	void testInstantiateAbstractClass() {
 		assertDetection("ClassNowAbstractMI.java", 8, CLASS_NOW_ABSTRACT, INSTANTIATION);
 	}
 
 	@Test
-	void testExtMethod() {
+	void testInstantiateAbstractClassParams() {
 		assertDetection("ClassNowAbstractMI.java", 12, CLASS_NOW_ABSTRACT, INSTANTIATION);
+	}
+
+	@Test
+	void testInstantiateInterface() {
+		assertDetection("IClassNowAbstractMI.java", 8, CLASS_NOW_ABSTRACT, INSTANTIATION);
+	}
+
+	@Test
+	void testInstantiateInterfaceParams() {
+		assertDetection("IClassNowAbstractMI.java", 12, CLASS_NOW_ABSTRACT, INSTANTIATION);
 	}
 }
