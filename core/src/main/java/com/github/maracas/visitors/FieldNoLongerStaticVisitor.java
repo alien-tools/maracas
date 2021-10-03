01267b21f03cdf4a4cbf884b3272ef642bc01003
Thomas Degueule <thomas.degueule@labri.fr>
2021-10-03 12:25:52 +0200

    Fix FieldNoLongerStaticVisitor

--- core/src/main/java/com/github/maracas/visitors/FieldNoLongerStaticVisitor.java
+++ core/src/main/java/com/github/maracas/visitors/FieldNoLongerStaticVisitor.java
@@ -8,6 +8,7 @@ import spoon.reflect.code.CtFieldRead;
 import spoon.reflect.code.CtFieldWrite;
 import spoon.reflect.code.CtTypeAccess;
 import spoon.reflect.reference.CtFieldReference;
+import spoon.reflect.reference.CtTypeReference;
 
 /**
  * Detections of FIELD_NO_LONGER_STATIC are:
@@ -32,15 +33,29 @@ public class FieldNoLongerStaticVisitor extends BreakingChangeVisitor {
 	}
 
 	private <T> void visitCtFieldAccess(CtFieldAccess<T> fieldAccess) {
-		if (fRef.equals(fieldAccess.getVariable()) && isStaticAccess(fieldAccess))
+		// Need to handle the case where the static field of a parent class
+		// is accessed through its subclass: SubClass.staticFieldInSuperClass
+		CtTypeReference<?> accessedType = fieldAccess.getVariable().getDeclaringType();
+		CtTypeReference<?> refType = fRef.getDeclaringType();
+
+		if (
+			accessedType.isSubtypeOf(refType) &&
+			// Not a big fan of the simpleName comparison, but it should be safe and
+			// the line below refuses to match
+			fieldAccess.getVariable().getSimpleName().equals(fRef.getFieldDeclaration().getSimpleName()) &&
+			//Objects.equal(fieldAccess.getVariable().getFieldDeclaration(), fRef.getFieldDeclaration())
+			isStaticAccess(fieldAccess)
+		)
 			detection(fieldAccess, fieldAccess.getVariable(), fRef, APIUse.FIELD_ACCESS);
 	}
 
 	private <T> boolean isStaticAccess(CtFieldAccess<T> fieldAccess) {
+		// Target is a CtTypeAccess for static fields
+		// and a CtExpression for everything else
 		if (fieldAccess.getTarget() instanceof CtTypeAccess<?> ta) {
 			// In a subclass, direct (unprefixed) access to a parent static field would
 			// still be counted as a TypeAccess. We check if it's actually accessed
-			// in a static way with isImplicit()
+			// in an (explicit) static way with isImplicit()
 			return !ta.isImplicit();
 		}
 
