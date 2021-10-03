7c9b8a35c456cd408175ba568ff5b74a76976ad9
Thomas Degueule <thomas.degueule@labri.fr>
2021-10-03 16:49:44 +0200

    Fix MethodRemovedTests

--- core/src/test/java/com/github/maracas/compchangestests/MethodRemovedTests.java
+++ core/src/test/java/com/github/maracas/compchangestests/MethodRemovedTests.java
@@ -1,16 +1,15 @@
 package com.github.maracas.compchangestests;
 
-import static com.github.maracas.detection.APIUse.*;
+import static com.github.maracas.detection.APIUse.METHOD_INVOCATION;
+import static com.github.maracas.detection.APIUse.METHOD_OVERRIDE;
 import static japicmp.model.JApiCompatibilityChange.METHOD_REMOVED;
 
 import org.junit.jupiter.api.Test;
 
 class MethodRemovedTests extends CompChangesTest {
-	// We're just checking the cases related to METHOD_REMOVED directly,
-	// not those caused by other removals
 	@Test
 	void testNoMore() {
-		assertNumberDetections(METHOD_REMOVED, 6);
+		assertNumberDetections(METHOD_REMOVED, 19);
 	}
 
 	@Test
@@ -41,5 +40,72 @@ class MethodRemovedTests extends CompChangesTest {
 	@Test
 	void testOverrideRemovedImpl() {
 		assertDetection("MethodRemovedImp.java", 8, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	// METHOD_REMOVED detected because the containing classes are removed
+	// or because it was removed from a super class
+	@Test
+	void testInvokeRemovedFromClass() {
+		assertDetection("ClassRemovedImp.java", 8, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	@Test
+	void testOverrideRemovedInSuper1() {
+		assertDetection("MethodRemovedInSuperclassExt.java", 8, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	@Test
+	void testOverrideRemovedInSuper2() {
+		assertDetection("MethodRemovedInSuperclassExt.java", 13, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	@Test
+	void testInvokeRemovedInSuper1() {
+		assertDetection("MethodRemovedInSuperclassExt.java", 18, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeRemovedInSuper2() {
+		assertDetection("MethodRemovedInSuperclassExt.java", 22, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testOverrideRemovedInSuperExt1() {
+		assertDetection("SMethodRemovedInSuperclassExt.java", 8, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	@Test
+	void testOverrideRemovedInSuperExt2() {
+		assertDetection("SMethodRemovedInSuperclassExt.java", 13, METHOD_REMOVED, METHOD_OVERRIDE);
+	}
+
+	@Test
+	void testInvokeRemovedInSuperExt1() {
+		assertDetection("SMethodRemovedInSuperclassExt.java", 18, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeRemovedInSuperExt2() {
+		assertDetection("SMethodRemovedInSuperclassExt.java", 22, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeMethodRemovedInSuper1() {
+		assertDetection("MethodRemovedInSuperclassFA.java", 10, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeMethodRemovedInSuper2() {
+		assertDetection("MethodRemovedInSuperclassFA.java", 15, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeMethodRemovedInSuper3() {
+		assertDetection("MethodRemovedInSuperclassFA.java", 20, METHOD_REMOVED, METHOD_INVOCATION);
+	}
+
+	@Test
+	void testInvokeMethodRemovedInSuper4() {
+		assertDetection("MethodRemovedInSuperclassFA.java", 25, METHOD_REMOVED, METHOD_INVOCATION);
 	}
 }
