429ec70e3d8ed8b11bff785eb0996cce2ce589c9
Lina Ochoa <lina.m8a@gmail.com>
2022-02-09 15:36:07 +0100

    Add validator project mvn skeleton

---
+++ validator/src/main/java/com/github/maracas/ValidatorCLI.java
@@ -0,0 +1,13 @@
+package com.github.maracas;
+
+/**
+ * Hello world!
+ *
+ */
+public class ValidatorCLI 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
