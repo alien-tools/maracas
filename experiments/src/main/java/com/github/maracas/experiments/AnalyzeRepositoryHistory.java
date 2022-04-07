82769d0af88c676f516c743ba0f85e604feaf321
Thomas Degueule <thomas.degueule@labri.fr>
2022-04-07 11:22:17 +0200

    WIP Experiments

--- experiments/src/main/java/com/github/maracas/experiments/AnalyzeRepositoryHistory.java
+++ experiments/src/main/java/com/github/maracas/experiments/AnalyzeRepositoryHistory.java
@@ -11,13 +11,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kohsuke.github.GHIssueState;
 import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class AnalyzeRepositoryHistory {
@@ -28,31 +31,47 @@ public class AnalyzeRepositoryHistory {
 
 	private final GitHub gh;
 	private final Forge forge;
+	private final GHRepository ghRepository;
+	private final Repository forgeRepository;
+	private final Map<String, Repository> clientRepositories = new HashMap<>();
+	private final ClientsManager clientsManager = new ClientsManager();
 
 	private final Logger logger = LogManager.getLogger(AnalyzeRepositoryHistory.class);
 
-	public AnalyzeRepositoryHistory(String owner, String name) throws IOException {
+	public AnalyzeRepositoryHistory(String owner, String name, List<String> clients) throws IOException {
+		logger.info("Fetching all repos");
 		this.owner = owner;
 		this.name = name;
 		this.fullName = owner + "/" + name;
 
 		this.gh = GitHubBuilder.fromEnvironment().build();
 		this.forge = new GitHubForge(gh);
+		this.ghRepository = gh.getRepository(fullName);
+		this.forgeRepository = forge.fetchRepository(owner, name);
+
+		clients.forEach(c -> {
+			String[] f = c.split("/");
+			this.clientRepositories.put(c, forge.fetchRepository(f[0], f[1]));
+			try {
+				clientsManager.addClient(c, gh.getRepository(c));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+		logger.info("Done");
 	}
 
 	public void analyze() throws Exception {
 		logger.info("Fetching PRs for {}", fullName);
-		var ghRepo = gh.getRepository(fullName);
-		var forgeRepo = forge.fetchRepository(owner, name);
 		var prs =
-			ghRepo.getPullRequests(GHIssueState.ALL)
+			ghRepository.getPullRequests(GHIssueState.ALL)
 				.stream()
 				.filter(this::affectsJavaFiles)
 				.limit(5)
 				.toList();
 
 		logger.info("Fetching releases for {}", fullName);
-		var releases = ghRepo.listReleases().toList();
+		var releases = ghRepository.listReleases().toList();
 
 		logger.info("Analyzing {}: {} PRs and {} releases", fullName, prs.size(), releases.size());
 		ForgeAnalyzer analyzer = new ForgeAnalyzer();
@@ -62,7 +81,7 @@ public class AnalyzeRepositoryHistory {
 			logger.info("[{}/{}] Analyzing {}#{} [{}]: {}", i, prs.size(), fullName, pr.getNumber(), pr.getState(), pr.getTitle());
 
 			Path wd = CLONE_PATH.resolve(String.valueOf(pr.getNumber()));
-			var forgePr = forge.fetchPullRequest(forgeRepo, pr.getNumber());
+			var forgePr = forge.fetchPullRequest(forgeRepository, pr.getNumber());
 			var buildV1 = new CommitBuilder(forgePr.prBase(), wd.resolve("base"));
 			var buildV2 = new CommitBuilder(forgePr.head(), wd.resolve("head"));
 			Properties props = new Properties();
@@ -74,7 +93,11 @@ public class AnalyzeRepositoryHistory {
 			props.setProperty("checkstyle.skip", "true");
 			buildV1.setBuildProperties(props);
 			buildV2.setBuildProperties(props);
-			var buildClients = fetchClients();
+			var clientCommits = clientsManager.clientsAtDate(pr.getClosedAt());
+			var buildClients = buildersFor(clientCommits);
+
+			logger.info("Potentially impacted clients [{}/{}]:", clientCommits.size(), clientRepositories.size());
+			clientCommits.keySet().forEach(name -> logger.info("\t{}@{}", name, clientCommits.get(name)));
 
 			AnalysisResult result = analyzer.analyzeCommits(buildV1, buildV2, buildClients, MaracasOptions.newDefault());
 			logger.info(result);
@@ -85,49 +108,58 @@ public class AnalyzeRepositoryHistory {
 		try {
 			return pr.listFiles().toList().stream().anyMatch(f -> f.getFilename().endsWith(".java"));
 		} catch (IOException e) {
+			e.printStackTrace();
 			return false;
 		}
 	}
 
-	public List<CommitBuilder> fetchClients() {
-		return List.of(
-			"SpoonLabs/flacoco",
-			"SpoonLabs/coming",
-			"SpoonLabs/astor",
-			"SpoonLabs/npefix",
-			"SpoonLabs/nopol",
-			"STAMP-project/AssertFixer",
-			"Spirals-Team/casper",
-			"SpoonLabs/CoCoSpoon",
-			"STAMP-project/dspot",
-			"SpoonLabs/gumtree-spoon-ast-diff",
-			"Spirals-Team/jPerturb",
-			"SpoonLabs/metamutator",
-			"SpoonLabs/spooet",
-			"KTH/spork"
-		).stream().map(c -> {
-			String[] f = c.split("/");
-			var clientRepo = forge.fetchRepository(f[0], f[1]);
-			Path wd =
-				CLONE_PATH
-				.resolve("clients")
-				.resolve(f[1])
-				.resolve("HEAD");
-			var builder = new CommitBuilder(forge.fetchCommit(clientRepo, "HEAD"), wd);
+	public List<CommitBuilder> buildersFor(Map<String, String> clientCommits) {
+		return clientCommits.keySet().stream()
+			.map(clientName -> {
+				String[] f = clientName.split("/");
+				var clientRepo = clientRepositories.get(clientName);
+				var commitSha = clientCommits.get(clientName);
+				var commit = forge.fetchCommit(clientRepo, commitSha);
+				var wd =
+					CLONE_PATH
+						.resolve("clients")
+						.resolve(f[0])
+						.resolve(f[1])
+						.resolve(commitSha);
+				var builder = new CommitBuilder(commit, wd);
 
-			// FIXME ;)
-			if ("dspot".equals(f[1]))
-				builder.setSources(Paths.get("dspot/src/main/java"));
-			if ("nopol".equals(f[1]))
-				builder.setSources(Paths.get("nopol/src/main/java"));
+				// FIXME: ;)
+				if ("dspot".equals(f[1]))
+					builder.setSources(Paths.get("dspot/src/main/java"));
+				if ("nopol".equals(f[1]))
+					builder.setSources(Paths.get("nopol/src/main/java"));
 
-			return builder;
-		}).toList();
+				return builder;
+			})
+			.toList();
 	}
 
 	public static void main(String[] args) {
 		try {
-			new AnalyzeRepositoryHistory("INRIA", "spoon").analyze();
+			new AnalyzeRepositoryHistory(
+				"INRIA", "spoon",
+				List.of(
+					"SpoonLabs/flacoco",
+					"SpoonLabs/coming",
+					"SpoonLabs/astor",
+					"SpoonLabs/npefix",
+					"SpoonLabs/nopol",
+					"STAMP-project/AssertFixer",
+					"Spirals-Team/casper",
+					"SpoonLabs/CoCoSpoon",
+					"STAMP-project/dspot",
+					"SpoonLabs/gumtree-spoon-ast-diff",
+					"Spirals-Team/jPerturb",
+					"SpoonLabs/metamutator",
+					"SpoonLabs/spooet",
+					"KTH/spork"
+				)
+			).analyze();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
---
+++ experiments/src/main/java/com/github/maracas/experiments/ClientsManager.java
@@ -0,0 +1,53 @@
+package com.github.maracas.experiments;
+
+import com.github.maracas.forges.CommitBuilder;
+import org.kohsuke.github.GHRepository;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.stream.Collectors.toMap;
+
+public class ClientsManager {
+	private final Map<String, GHRepository> clients = new HashMap<>();
+
+	public void addClient(String name, GHRepository repo) {
+		clients.put(name, repo);
+	}
+
+	public Map<String, String> clientsAtDate(Date closedAt) {
+		// TODO: check whether the client was already depending on the library at this point, in the right version
+		return clients.values().stream()
+			.filter(c -> existsAt(c, closedAt))
+			.map(c -> {
+				try {
+					var commits = c.queryCommits().until(closedAt).list().toList();
+
+					if (!commits.isEmpty())
+						return commits.get(commits.size() - 1);
+					else
+						return null;
+				} catch (IOException e) {
+					e.printStackTrace();
+					return null;
+				}
+			})
+			.filter(Objects::nonNull)
+			.collect(toMap(
+				c -> c.getOwner().getFullName(),
+				c -> c.getSHA1()
+			));
+	}
+
+	private boolean existsAt(GHRepository repo, Date date) {
+		try  {
+			return repo.getCreatedAt().before(date);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
