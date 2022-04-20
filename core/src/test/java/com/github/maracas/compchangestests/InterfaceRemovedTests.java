285cfab7f5e161e0cde0c0fb46b939fbe57ee1c4
Lina Ochoa <lina.m8a@gmail.com>
2022-04-20 14:32:38 +0200

    Add new interface removed test cases

--- core/src/test/java/com/github/maracas/compchangestests/InterfaceRemovedTests.java
+++ core/src/test/java/com/github/maracas/compchangestests/InterfaceRemovedTests.java
@@ -5,6 +5,7 @@ import static com.github.maracas.brokenuse.APIUse.METHOD_INVOCATION;
 import static com.github.maracas.brokenuse.APIUse.METHOD_OVERRIDE;
 import static com.github.maracas.brokenuse.APIUse.TYPE_DEPENDENCY;
 import static japicmp.model.JApiCompatibilityChange.INTERFACE_REMOVED;
+import static japicmp.model.JApiCompatibilityChange.SUPERCLASS_REMOVED;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -13,7 +14,7 @@ public class InterfaceRemovedTests extends CompChangesTest {
 
     @Test
     void testNoMore() {
-        assertNumberBrokenUses(INTERFACE_REMOVED, 7);
+        assertNumberBrokenUses(INTERFACE_REMOVED, 9);
     }
 
     @Disabled("Issue with isSubtypeOf")
@@ -22,16 +23,66 @@ public class InterfaceRemovedTests extends CompChangesTest {
         assertBrokenUse("InterfaceRemovedImp.java", 8, INTERFACE_REMOVED, METHOD_OVERRIDE);
     }
 
+    @Test
+    void testImplementsInterSubtype() {
+        assertNoBrokenUse("InterfaceRemovedImp.java", 13, INTERFACE_REMOVED, METHOD_OVERRIDE);
+    }
+
     @Disabled("Issue with isSubtypeOf")
     @Test
     void testImplementsInterMulti() {
         assertBrokenUse("InterfaceRemovedImpMulti.java", 13, INTERFACE_REMOVED, METHOD_OVERRIDE);
     }
 
+    @Test
+    void testImplementsInterMultiSubtype1() {
+        assertNoBrokenUse("InterfaceRemovedImpMulti.java", 8, INTERFACE_REMOVED, METHOD_OVERRIDE);
+    }
+
+    @Test
+    void testImplementsInterMultiSubtype2() {
+        assertNoBrokenUse("InterfaceRemovedImpMulti.java", 18, INTERFACE_REMOVED, METHOD_OVERRIDE);
+    }
+
     @Disabled("Issue with isSubtypeOf")
     @Test
     void testExtendsAbsClass() {
     	assertBrokenUse("InterfaceAddedExtAbs.java", 5, INTERFACE_REMOVED, METHOD_OVERRIDE);
+    }
+
+    @Test
+    void testExtendsCastVar() {
+        assertBrokenUse("InterfaceRemovedExt.java", 12, INTERFACE_REMOVED, TYPE_DEPENDENCY);
+    }
+
+    @Test
+    void testExtendsSubtypeFieldAccessCTE() {
+        assertBrokenUse("InterfaceRemovedExt.java", 16, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+    void testExtendsSubtypeStaticFieldAccessCTE() {
+        assertNoBrokenUse("InterfaceRemovedExt.java", 20, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+    void testExtendsDirectFieldAccessCTE() {
+        assertBrokenUse("InterfaceRemovedExt.java", 24, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+    void testExtendsSubtypeFieldAccessList() {
+        assertBrokenUse("InterfaceRemovedExt.java", 28, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+    void testExtendsSubtypeStaticFieldAccessList() {
+        assertNoBrokenUse("InterfaceRemovedExt.java", 32, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+    void testExtendsDirectFieldAccessList() {
+        assertBrokenUse("InterfaceRemovedExt.java", 36, INTERFACE_REMOVED, FIELD_ACCESS);
     }
 
     @Test
@@ -44,35 +95,6 @@ public class InterfaceRemovedTests extends CompChangesTest {
         assertBrokenUse("InterfaceRemovedExt.java", 48, INTERFACE_REMOVED, METHOD_INVOCATION);
     }
 
-    @Disabled("Shall we report this case or not?")
-    @Test
-    void testExtendsCastVar() {
-        assertNoBrokenUse("InterfaceRemovedExt.java", 12, INTERFACE_REMOVED, TYPE_DEPENDENCY);
-    }
-
-    @Test
-    void testExtendsSubtypeFieldAccessCTE() {
-        assertBrokenUse("InterfaceRemovedExt.java", 16, INTERFACE_REMOVED, FIELD_ACCESS);
-    }
-
-    @Test
-    void testExtendsSubtypeFieldAccessList() {
-        assertBrokenUse("InterfaceRemovedExt.java", 28, INTERFACE_REMOVED, FIELD_ACCESS);
-    }
-
-    @Disabled("No implementation yet!")
-    @Test
-    void testExtendsDirectFieldAccessCTE() {
-        assertBrokenUse("InterfaceRemovedExt.java", 24, INTERFACE_REMOVED, FIELD_ACCESS);
-    }
-
-    @Disabled("No implementation yet!")
-    @Test
-    void testExtendsDirectFieldAccessList() {
-        assertBrokenUse("InterfaceRemovedExt.java", 36, INTERFACE_REMOVED, FIELD_ACCESS);
-    }
-
-    @Disabled("Shall we report this case or not?")
     @Test
     void testCastVar() {
         assertBrokenUse("InterfaceRemovedTD.java", 12, INTERFACE_REMOVED, TYPE_DEPENDENCY);
@@ -84,7 +106,22 @@ public class InterfaceRemovedTests extends CompChangesTest {
     }
 
     @Test
+    void testSubtypeStaticFieldAccessCTE() {
+        assertNoBrokenUse("InterfaceRemovedTD.java", 20, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
     void testSubtypeFieldAccessList() {
         assertBrokenUse("InterfaceRemovedTD.java", 24, INTERFACE_REMOVED, FIELD_ACCESS);
     }
+
+    @Test
+    void testSubtypeStaticFieldAccessList() {
+        assertNoBrokenUse("InterfaceRemovedTD.java", 28, INTERFACE_REMOVED, FIELD_ACCESS);
+    }
+
+    @Test
+	void testSubtypeInv() {
+		assertNoBrokenUse("SuperclassRemovedTD.java", 33, SUPERCLASS_REMOVED, METHOD_INVOCATION);
+	}
 }
