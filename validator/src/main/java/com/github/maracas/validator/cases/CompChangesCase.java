e18aefebbc3a1ec72f51843ba20f201428e8206b
Lina Ochoa <lina.m8a@gmail.com>
2022-04-25 10:22:30 +0200

    Move default options validator

--- validator/src/main/java/com/github/maracas/validator/cases/CompChangesCase.java
+++ validator/src/main/java/com/github/maracas/validator/cases/CompChangesCase.java
@@ -8,9 +8,6 @@ import com.github.maracas.validator.MaracasValidator;
 import com.github.maracas.validator.build.MavenArtifactUpgrade;
 import com.github.maracas.validator.matchers.MatcherFilter;
 import com.github.maracas.validator.matchers.MatcherOptions;
-import com.github.maracas.validator.matchers.MissingJApiCompatibilityChange;
-
-import japicmp.model.JApiCompatibilityChange;
 
 /**
  * Maracas validator on the CompChanges project.
@@ -29,7 +26,7 @@ public class CompChangesCase {
         MavenArtifactUpgrade upgrade = new MavenArtifactUpgrade(null, null,
             "comp-changes-old", "comp-changes-new", null, null);
 
-        MatcherOptions opts = compChangesOptions();
+        MatcherOptions opts = MatcherOptions.defaultOptions();
         MaracasValidator validator = new MaracasValidator(srcApi1, srcApi2, srcClient,
             null, upgrade, opts, true);
         MatcherFilter filter = new CompChangesMatcherFilter(opts);
@@ -41,43 +38,5 @@ public class CompChangesCase {
 
         // Create report
         validator.createHTMLReport(report);
-    }
-
-    /**
-     * Returns a default instance of the class excluding unsupported breaking
-     * changes in Maracas core.
-     *
-     * @return default matcher options
-     */
-    private static MatcherOptions compChangesOptions() {
-        MatcherOptions opts = new MatcherOptions();
-
-        // Coming from Maracas broken uses (unimplemented)
-        opts.excludeBreakingChange(JApiCompatibilityChange.CLASS_NO_LONGER_PUBLIC, "classNoLongerPublic");
-        opts.excludeBreakingChange(JApiCompatibilityChange.CLASS_TYPE_CHANGED, "classTypeChanged");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_NOW_DEFAULT, "methodAbstractNowDefault");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_LESS_ACCESSIBLE, "methodLessAccessible");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NO_LONGER_STATIC, "methodNoLongerStatic");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NOW_STATIC, "methodNowStatic");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NOW_THROWS_CHECKED_EXCEPTION, "methodNowThrosCheckedException");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NO_LONGER_THROWS_CHECKED_EXCEPTION, "methodNoLongerThrowsCheckedException");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_TO_CLASS, "methodAbstractAddedToClass");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NEW_DEFAULT, "methodNewDefault");
-        opts.excludeBreakingChange(JApiCompatibilityChange.CONSTRUCTOR_LESS_ACCESSIBLE, "constructorLessAccessible");
-
-        // Coming from Maracas delta by design
-        opts.excludeBreakingChange(JApiCompatibilityChange.SUPERCLASS_MODIFIED_INCOMPATIBLE, "superclassModifiedIncompatible");
-        //opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_REMOVED_IN_SUPERCLASS, "methodRemovedInSuperclass");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ADDED_TO_PUBLIC_CLASS, "methodAddedToPublicClass");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_IN_SUPERCLASS, "methodAbstractAddedInSuperclass");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_IN_IMPLEMENTED_INTERFACE, "methodAbstractAddedInImplementedInterface");
-        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_DEFAULT_ADDED_IN_IMPLEMENTED_INTERFACE, "methodDefaultAddedInImplementedInterface");
-        opts.excludeBreakingChange(JApiCompatibilityChange.FIELD_LESS_ACCESSIBLE_THAN_IN_SUPERCLASS, "fieldLessAccessibleThanInSuperclass");
-        //opts.excludeBreakingChange(JApiCompatibilityChange.FIELD_REMOVED_IN_SUPERCLASS, "fieldRemovedInSuperclass");
-
-        // Missing JApiCmp breaking changes
-        opts.excludeBreakingChange(MissingJApiCompatibilityChange.METHOD_MORE_ACCESSIBLE, "methodMoreAccessible");
-
-        return opts;
     }
 }
--- validator/src/main/java/com/github/maracas/validator/matchers/MatcherOptions.java
+++ validator/src/main/java/com/github/maracas/validator/matchers/MatcherOptions.java
@@ -38,4 +38,42 @@ public record MatcherOptions(
     public void excludeBreakingChange(Enum change, String regex) {
         excludedBreakingChanges.put(change, regex);
     }
+
+    /**
+     * Returns a default instance of the class excluding unsupported breaking
+     * changes in Maracas core. It shows the current state of Maracas implementation.
+     *
+     * @return default matcher options
+     */
+    public static MatcherOptions defaultOptions() {
+    	MatcherOptions opts = new MatcherOptions();
+
+        // Coming from Maracas broken uses (unimplemented)
+        opts.excludeBreakingChange(JApiCompatibilityChange.CLASS_NO_LONGER_PUBLIC, "classNoLongerPublic");
+        opts.excludeBreakingChange(JApiCompatibilityChange.CLASS_TYPE_CHANGED, "classTypeChanged");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_NOW_DEFAULT, "methodAbstractNowDefault");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_LESS_ACCESSIBLE, "methodLessAccessible");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NO_LONGER_STATIC, "methodNoLongerStatic");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NOW_STATIC, "methodNowStatic");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NOW_THROWS_CHECKED_EXCEPTION, "methodNowThrosCheckedException");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NO_LONGER_THROWS_CHECKED_EXCEPTION, "methodNoLongerThrowsCheckedException");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_TO_CLASS, "methodAbstractAddedToClass");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_NEW_DEFAULT, "methodNewDefault");
+        opts.excludeBreakingChange(JApiCompatibilityChange.CONSTRUCTOR_LESS_ACCESSIBLE, "constructorLessAccessible");
+
+        // Coming from Maracas delta by design
+        opts.excludeBreakingChange(JApiCompatibilityChange.SUPERCLASS_MODIFIED_INCOMPATIBLE, "superclassModifiedIncompatible");
+        //opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_REMOVED_IN_SUPERCLASS, "methodRemovedInSuperclass");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ADDED_TO_PUBLIC_CLASS, "methodAddedToPublicClass");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_IN_SUPERCLASS, "methodAbstractAddedInSuperclass");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_ABSTRACT_ADDED_IN_IMPLEMENTED_INTERFACE, "methodAbstractAddedInImplementedInterface");
+        opts.excludeBreakingChange(JApiCompatibilityChange.METHOD_DEFAULT_ADDED_IN_IMPLEMENTED_INTERFACE, "methodDefaultAddedInImplementedInterface");
+        opts.excludeBreakingChange(JApiCompatibilityChange.FIELD_LESS_ACCESSIBLE_THAN_IN_SUPERCLASS, "fieldLessAccessibleThanInSuperclass");
+        //opts.excludeBreakingChange(JApiCompatibilityChange.FIELD_REMOVED_IN_SUPERCLASS, "fieldRemovedInSuperclass");
+
+        // Missing JApiCmp breaking changes
+        opts.excludeBreakingChange(MissingJApiCompatibilityChange.METHOD_MORE_ACCESSIBLE, "methodMoreAccessible");
+
+        return opts;
+    }
 }
