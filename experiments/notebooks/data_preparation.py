c5331d121dfe8416089881e1527c8bcc58da10b3
Lina Ochoa <lina.m8a@gmail.com>
2022-08-08 10:45:25 +0200

    Finalize data preparation scripts

--- experiments/notebooks/data_preparation.py
+++ experiments/notebooks/data_preparation.py
@@ -5,13 +5,12 @@ import pandas as pd
 
 def compute_url_parts(url_col: str) -> tuple[str, str]:
     """
-    Returns the GitHub scheme and authority of the data 
-    frame column.
-    :param url_col: name of the column in the data frame. Can 
-        be 'sshUrl', 'url', 'csshUrl', or 'curl'
-    :returns: tuple whose first element is the scheme of the
-        expected urls (e.g. 'ssh', 'https') and its second 
-        element is the authority of the expected urls.
+    Returns the GitHub scheme and authority of the data frame column.
+    :param url_col: name of the column in the data frame. Can be 'sshUrl', 
+        'url', 'csshUrl', or 'curl'
+    :returns: tuple whose first element is the scheme of the expected 
+        urls (e.g. 'ssh', 'https') and its second element is the authority 
+        of the expected urls.
     :raises: `ValueError` if the url_col is invalid.
     """
     if url_col in ['sshUrl', 'csshUrl']:
@@ -24,14 +23,13 @@ def compute_url_parts(url_col: str) -> tuple[str, str]:
         
 def validate_urls(url_col: str, df: pd.DataFrame) -> bool:
     """
-    Validates if the values of a given URL column within the
-    data frame are valid. Concretely, it performs three checks:
-    1. all URLs start with the expected scheme; 2. all URLs 
-    contain the expected authority, and; 3. all URLs are built 
-    in the right way and contain the expected path based on the 
-    repository owner and name values.
-    :param url_col: name of the column in the data frame. Can 
-        be 'sshUrl', 'url', 'csshUrl', or 'curl'
+    Validates if the values of a given URL column within the data frame 
+    are valid. Concretely, it performs three checks: 1. all URLs start 
+    with the expected scheme; 2. all URLs contain the expected authority, 
+    and; 3. all URLs are built in the right way and contain the expected 
+    path based on the repository owner and name values.
+    :param url_col: name of the column in the data frame. Can be 'sshUrl', 
+        'url', 'csshUrl', or 'curl'
     :param df: target data frame
     :returns: `True` if all checks pass, `False` otherwise.
     """
@@ -59,7 +57,9 @@ def validate_urls(url_col: str, df: pd.DataFrame) -> bool:
     print(f'Records have right authority: {authority_check}')
 
     # SSH URLs have the right path
-    mask = df[url_col] == f'{scheme}://{authority}{delimiter}' + df[owner_col].map(str) + '/' + df[name_col].map(str) + extension
+    mask = df[url_col] == f'{scheme}://{authority}{delimiter}' \
+        + df[owner_col].map(str) + '/' \
+        + df[name_col].map(str) + extension
     path_check = mask.all()
     print(f'Records have right path: {path_check}')
     
@@ -68,16 +68,15 @@ def validate_urls(url_col: str, df: pd.DataFrame) -> bool:
 
 def validate_date(date_col: str, df: pd.DataFrame) -> bool:
     """
-    Validates if the values of a given date column within the 
-    data frame are valid. Concretely, it checks if all dates 
-    are of the format yyyy-MM-dd.
-    :param date_col: name of the column in the data frame. Can 
-        be 'createdAt' or 'pushedAt'
+    Validates if the values of a given date column within the data frame 
+    are valid. Concretely, it checks if all dates are of the format yyyy-MM-dd.
+    :param date_col: name of the column in the data frame. Can be 'createdAt', 
+        'pushedAt', or 'publishedAt'
     :param df: target data frame
     :returns: `True` if the check passes, `False` otherwise.
     :raises: `ValueError` if the date_col is invalid.
     """
-    if date_col not in ['createdAt', 'pushedAt']:
+    if date_col not in ['createdAt', 'pushedAt', 'publishedAt']:
         raise ValueError('Invalid date_col value')
         
     mask = df[date_col].str.match('^\d{4}-\d{2}-\d{2}')
@@ -89,18 +88,17 @@ def validate_date(date_col: str, df: pd.DataFrame) -> bool:
 
 def validate_limit_date(limit_date: datetime, date_col: str, df: pd.DataFrame) -> bool:
     """
-    Validates if the values of a given date column within the 
-    data frame are valid. Concretely, it performs two checks: 
-    1. all date appear after a limit date, and; 2. all dates 
-    appear before the present date.
+    Validates if the values of a given date column within the data frame 
+    are valid. Concretely, it performs two checks: 1. all date appear 
+    after a limit date, and; 2. all dates appear before the present date.
     :param limit_date: limit past date for all records
-    :param date_col: name of the column in the data frame. Can 
-        be 'createdAt' or 'pushedAt'
+    :param date_col: name of the column in the data frame. Can be 'createdAt', 
+        'pushedAt', or 'publishedAt'
     :param df: target data frame
     :returns: `True` if all checks pass, `False` otherwise.
     :raises: `ValueError` if the date_col is invalid.
     """ 
-    if date_col not in ['createdAt', 'pushedAt']:
+    if date_col not in ['createdAt', 'pushedAt', 'publishedAt']:
         raise ValueError('Invalid date_col value')
     
     mask = pd.to_datetime(df[date_col]) >= limit_date
@@ -117,18 +115,20 @@ def validate_limit_date(limit_date: datetime, date_col: str, df: pd.DataFrame) -
 
 def validate_limit_num(limit_num: int, num_col: str, df: pd.DataFrame) -> bool:
     """
-    Validates if the values of a given numerical column within the 
-    data frame are valid. Concretely, it checks if they are greater 
-    than or equal to a limit number.
+    Validates if the values of a given numerical column within the data 
+    frame are valid. Concretely, it checks if they are greater than or 
+    equal to a limit number.
     :param limit_num: limit number
-    :param num_col: name of the column in the data frame. Can 
-        be 'stars', 'packages', 'clients', 'relevantClients' or 'cstars'
+    :param num_col: name of the column in the data frame. Can be 'stars', 
+        'packages', 'clients', 'relevantClients', 'cstars', 'files', or 
+        'number'
     :param df: target data frame
     :returns: `True` if the check passes, `False` otherwise.
     :raises: `ValueError` if the num_col is invalid.
     """
-    if num_col not in ['stars', 'packages', 'clients', 'relevantClients', 'cstars', 'files']:
-        raise ValueError('Invalid num_colnum_col value')
+    if num_col not in ['stars', 'packages', 'clients', 'relevantClients', 
+                       'cstars', 'files', 'number']:
+        raise ValueError('Invalid num_col value')
     
     mask = df[num_col] >= limit_num
     limit_check = mask.all()
@@ -138,6 +138,19 @@ def validate_limit_num(limit_num: int, num_col: str, df: pd.DataFrame) -> bool:
 
 
 def validate_repo_name(repo_col: str, df: pd.DataFrame) -> bool:
+    """
+    Validates if the values of a given repository column within the data 
+    frame are valid. Concretely, it checks if the repository name contains 
+    the expected path based on the repository owner and name values (i.e.,
+    'owner/name').
+    :param repo_col: name of the column in the data frame. Can be 'baseRepo'.
+    :param df: target data frame
+    :returns: `True` if the check passes, `False` otherwise.
+    :raises: `ValueError` if the repo_col is invalid.
+    """
+    if repor_col not in ['baseRepo']:
+        raise ValueError('Invalid repo_col value')
+        
     mask = df[repo_col] == df['owner'].map(str) + '/' + df['name'].map(str)
     repo_name_check = mask.all()
     print(f'Records have right repository name: {repo_name_check}')
@@ -146,6 +159,19 @@ def validate_repo_name(repo_col: str, df: pd.DataFrame) -> bool:
 
 
 def validate_repo_name_format(repo_col: str, df: pd.DataFrame) -> bool:
+    """
+    Validates if the values of a given repository column within the data 
+    frame are valid. Concretely, it checks if the repository name follows 
+    the expected format (i.e., 'owner/name').
+    :param repo_col: name of the column in the data frame. Can be 'baseRepo'
+        or 'headRepo'.
+    :param df: target data frame
+    :returns: `True` if the check passes, `False` otherwise.
+    :raises: `ValueError` if the repo_col is invalid.
+    """
+    if repor_col not in ['baseRepo', 'headRepo']:
+        raise ValueError('Invalid repo_col value')
+        
     mask = df[repo_col].str.match('^[A-Za-z0-9._-]+/[A-Za-z0-9._-]+$')
     repo_name_check = mask.all()
     print(f'Records have right repository name format: {repo_name_check}')
@@ -154,6 +180,14 @@ def validate_repo_name_format(repo_col: str, df: pd.DataFrame) -> bool:
 
 
 def validate_value(value: any, col: str, df: pd.DataFrame) -> bool:
+    """
+    Validates if the values of a given column within the data frame
+    contain the same expected value.
+    :param value: expected value in the column
+    :para col: name of the column in the data frame
+    :param df: target data frame
+    :returns: `True` if the check passes, `False` otherwise.
+    """
     mask = df[col] == value
     value_check = mask.all()
     print(f'Records have value {value}: {value_check}')
