81b730e8cee0a8d536afc6056dd328071a469823
Thomas Degueule <thomas.degueule@labri.fr>
2022-09-28 01:49:32 +0200

    rest: dummy bootstrap UI for reports

--- rest/src/main/java/com/github/maracas/rest/data/PullRequestResponse.java
+++ rest/src/main/java/com/github/maracas/rest/data/PullRequestResponse.java
@@ -2,6 +2,7 @@ package com.github.maracas.rest.data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
 
 public record PullRequestResponse(
@@ -10,6 +11,11 @@ public record PullRequestResponse(
 ) {
 	public PullRequestResponse(String message) {
 		this(message, null);
+	}
+
+	public static PullRequestResponse fromJson(File json) throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, PullRequestResponse.class);
 	}
 
 	public String toJson() throws IOException {
