ede1ce98c3cdb89f11e3d24ee9b9767d18e2a33d
Thomas Degueule <thomas.degueule@labri.fr>
2023-02-28 18:11:45 +0100

    forges: rewrite GitHubForgeTest in terms of alien-tools/repository-fixture

--- forges/src/test/java/com/github/maracas/forges/github/GitHubForgeTest.java
+++ forges/src/test/java/com/github/maracas/forges/github/GitHubForgeTest.java
@@ -37,92 +37,92 @@ class GitHubForgeTest {
 
   @Test
   void fetchRepository_valid() {
-    Repository repo = github.fetchRepository("alien-tools", "maracas");
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture");
     assertEquals("alien-tools", repo.owner());
-    assertEquals("maracas", repo.name());
-    assertEquals("alien-tools/maracas", repo.fullName());
-    assertEquals("https://github.com/alien-tools/maracas.git", repo.remoteUrl());
+    assertEquals("repository-fixture", repo.name());
+    assertEquals("alien-tools/repository-fixture", repo.fullName());
+    assertEquals("https://github.com/alien-tools/repository-fixture.git", repo.remoteUrl());
     assertEquals("main", repo.branch());
   }
 
   @Test
   void fetchRepository_branch_valid() {
-    Repository repo = github.fetchRepository("alien-tools", "maracas", "main");
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture", "pr-on-a-branch");
     assertEquals("alien-tools", repo.owner());
-    assertEquals("maracas", repo.name());
-    assertEquals("alien-tools/maracas", repo.fullName());
-    assertEquals("https://github.com/alien-tools/maracas.git", repo.remoteUrl());
-    assertEquals("main", repo.branch());
+    assertEquals("repository-fixture", repo.name());
+    assertEquals("alien-tools/repository-fixture", repo.fullName());
+    assertEquals("https://github.com/alien-tools/repository-fixture.git", repo.remoteUrl());
+    assertEquals("pr-on-a-branch", repo.branch());
   }
 
   @Test
   void fetchRepository_branch_unknown() {
-    assertThrows(ForgeException.class, () -> github.fetchRepository("alien-tools", "maracas", "unknown"));
+    assertThrows(ForgeException.class, () -> github.fetchRepository("alien-tools", "repository-fixture", "unknown"));
   }
 
   @Test
   void fetchPullRequest_unknown() {
-    Repository repo = github.fetchRepository("alien-tools", "comp-changes");
-    assertThrows(ForgeException.class, () -> github.fetchPullRequest(repo, -1));
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture");
+    assertThrows(ForgeException.class, () -> github.fetchPullRequest(repo, 999));
   }
 
   @Test
   void fetchPullRequest_opened() {
-    Repository repo = github.fetchRepository("alien-tools", "comp-changes");
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture");
     PullRequest pr = github.fetchPullRequest(repo, 2);
     assertEquals(2, pr.number());
-    assertEquals(new Commit(repo, "43463c9c73933ae0e791dbf8d1d6e152101a4ba9"), pr.head());
-    assertEquals(new Commit(repo, "00dde47b0bf583c4a9320e2968d5fbad0af81265"), pr.mergeBase());
+    assertEquals(new Commit(repo, "fde03b35492be04013bc9cb7712ec3ad5d5a9214"), pr.head());
+    assertEquals(new Commit(repo, "15b08c0f6acba8fe369d0076c583fb22311f8524"), pr.mergeBase());
     assertEquals("main", pr.baseBranch());
-    assertEquals("prepare-v2", pr.headBranch());
+    assertEquals("pr-on-readme", pr.headBranch());
     assertEquals("alien-tools", pr.repository().owner());
-    assertEquals("comp-changes", pr.repository().name());
+    assertEquals("repository-fixture", pr.repository().name());
     assertEquals("main", pr.repository().branch());
-    assertEquals(237, pr.changedFiles().size());
+    assertEquals(1, pr.changedFiles().size());
   }
 
   @Test
   void fetchPullRequest_closed() {
-    Repository repo = github.fetchRepository("INRIA", "spoon");
-    PullRequest pr = github.fetchPullRequest(repo, 4625);
-    assertEquals(4625, pr.number());
-    assertEquals(new Commit(repo, "1ef7b095d58ff671b74f5eef7186c96aa573304e"), pr.head());
-    assertEquals(new Commit(repo, "6095f1ba4c6eb98ad5e251f9c5d0aaedff3a1637"), pr.mergeBase());
-    assertEquals("master", pr.baseBranch());
-    assertEquals("regression-resource", pr.headBranch());
-    assertEquals("INRIA", pr.repository().owner());
-    assertEquals("spoon", pr.repository().name());
-    assertEquals("master", pr.repository().branch());
-    assertEquals(6, pr.changedFiles().size());
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture");
+    PullRequest pr = github.fetchPullRequest(repo, 8);
+    assertEquals(8, pr.number());
+    assertEquals(new Commit(repo, "fde03b35492be04013bc9cb7712ec3ad5d5a9214"), pr.head());
+    assertEquals(new Commit(repo, "15b08c0f6acba8fe369d0076c583fb22311f8524"), pr.mergeBase());
+    assertEquals("pr-does-not-compile", pr.baseBranch());
+    assertEquals("pr-on-readme", pr.headBranch());
+    assertEquals("alien-tools", pr.repository().owner());
+    assertEquals("repository-fixture", pr.repository().name());
+    assertEquals("main", pr.repository().branch());
+    assertEquals(1, pr.changedFiles().size());
   }
 
   @Test
   void fetchPullRequest_that_was_synchronized() {
-    Repository repo = github.fetchRepository("javaparser", "javaparser");
-    PullRequest pr = github.fetchPullRequest(repo, 3320);
-    assertEquals(3320, pr.number());
-    assertEquals(new Commit(repo, "dfb238b9de62242d433decc118c19db1fbe0dd1d"), pr.head());
-    assertEquals(new Commit(repo, "c2531bbaa671c854bf57b8dc3bb679f20190ded3"), pr.mergeBase());
-    assertEquals("master", pr.baseBranch());
-    assertEquals("support-jimfs", pr.headBranch());
-    assertEquals("javaparser", pr.repository().owner());
-    assertEquals("javaparser", pr.repository().name());
-    assertEquals("master", pr.repository().branch());
+    Repository repo = github.fetchRepository("alien-tools", "repository-fixture");
+    PullRequest pr = github.fetchPullRequest(repo, 1);
+    assertEquals(1, pr.number());
+    assertEquals(new Commit(repo, "21a00987e142aee347c4df27836fe07909517b29"), pr.head());
+    assertEquals(new Commit(repo, "15b08c0f6acba8fe369d0076c583fb22311f8524"), pr.mergeBase());
+    assertEquals("main", pr.baseBranch());
+    assertEquals("pr-on-modules", pr.headBranch());
+    assertEquals("alien-tools", pr.repository().owner());
+    assertEquals("repository-fixture", pr.repository().name());
+    assertEquals("main", pr.repository().branch());
   }
 
   @Test
   void fetchCommit_unknown() {
-    assertThrows(ForgeException.class, () -> github.fetchCommit("alien-tools", "maracas", "unknown"));
+    assertThrows(ForgeException.class, () -> github.fetchCommit("alien-tools", "repository-fixture", "unknown"));
   }
 
   @Test
   void fetchCommit_valid() {
-    Commit c = github.fetchCommit("alien-tools", "maracas", "655f99bad85435c145fc816018962dc7644edb1f");
-    assertEquals("655f99bad85435c145fc816018962dc7644edb1f", c.sha());
+    Commit c = github.fetchCommit("alien-tools", "repository-fixture", "fec2de87113764cdfeee36c16c84ca3af0d323b9");
+    assertEquals("fec2de87113764cdfeee36c16c84ca3af0d323b9", c.sha());
     assertEquals("alien-tools", c.repository().owner());
-    assertEquals("maracas", c.repository().name());
-    assertEquals("alien-tools/maracas", c.repository().fullName());
-    assertEquals("https://github.com/alien-tools/maracas.git", c.repository().remoteUrl());
+    assertEquals("repository-fixture", c.repository().name());
+    assertEquals("alien-tools/repository-fixture", c.repository().fullName());
+    assertEquals("https://github.com/alien-tools/repository-fixture.git", c.repository().remoteUrl());
     assertEquals("main", c.repository().branch());
   }
 
