f0613dea8e414964e47b4b67e5e9e12301814335
Thomas Degueule <thomas.degueule@labri.fr>
2023-03-13 10:03:28 +0100

    rest: ClientsServiceTest: no Spring context, spoon -> drill

--- rest/src/test/java/com/github/maracas/rest/services/ClientsServiceTest.java
+++ rest/src/test/java/com/github/maracas/rest/services/ClientsServiceTest.java
@@ -4,86 +4,84 @@ import com.github.maracas.forges.Repository;
 import com.github.maracas.rest.breakbot.BreakbotConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.kohsuke.github.GitHubBuilder;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
 class ClientsServiceTest {
-	@Autowired
 	ClientsService clientsService;
-	Repository spoon;
+	Repository drill;
 
 	@BeforeEach
 	void setUp() {
-		spoon = new Repository("INRIA", "spoon", "", "");
+		try {
+			clientsService = new ClientsService(GitHubBuilder.fromEnvironment().build(), "./data-test/clients", 1);
+			drill = new Repository("apache", "drill", "", "");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	void test_spoon_top_10() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 0, Collections.emptyList());
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
-		assertThat(clients, hasSize(10));
+	void test_drill_top_5() {
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 0, Collections.emptyList());
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
+		assertThat(clients, hasSize(5));
 	}
 
 	@Test
-	void test_spoon_stars_100() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 100, Collections.emptyList());
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
-		assertThat(clients, not(empty()));
-		assertThat(clients, hasSize(lessThanOrEqualTo(10)));
-	}
-
-	@Test
-	void test_spoon_top_and_stars() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 1, Collections.emptyList());
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
-		assertThat(clients, hasSize(10));
-	}
-
-	@Test
-	void test_spoon_top_10_with_custom() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 0, Collections.singletonList(
-			new BreakbotConfig.GitHubRepository("a/b", "", "", "")
-		));
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
-		assertThat(clients, hasSize(11));
-	}
-
-	@Test
-	void test_spoon_stars_10000_with_custom() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 10000, Collections.singletonList(
-			new BreakbotConfig.GitHubRepository("a/b", "", "", "")
-		));
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
+	void test_drill_stars_100() {
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 100, Collections.emptyList());
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
 		assertThat(clients, hasSize(equalTo(1)));
 	}
 
 	@Test
-	void test_spoon_only_custom() {
+	void test_drill_top_and_stars() {
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 1, Collections.emptyList());
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
+		assertThat(clients, hasSize(5));
+	}
+
+	@Test
+	void test_drill_top_5_with_custom() {
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 0, Collections.singletonList(
+			new BreakbotConfig.GitHubRepository("a/b", "", "", "")
+		));
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
+		assertThat(clients, hasSize(6));
+	}
+
+	@Test
+	void test_drill_stars_10000_with_custom() {
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 10000, Collections.singletonList(
+			new BreakbotConfig.GitHubRepository("a/b", "", "", "")
+		));
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
+		assertThat(clients, hasSize(equalTo(1)));
+	}
+
+	@Test
+	void test_drill_only_custom() {
 		BreakbotConfig.Clients config = new BreakbotConfig.Clients(0, 0, Collections.singletonList(
 			new BreakbotConfig.GitHubRepository("a/b", "", "", "")
 		));
-		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(spoon, config, "fr.inria.gforge.spoon:spoon-core");
+		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(drill, config, "org.apache.drill.exec:drill-rpc");
 		assertThat(clients, hasSize(equalTo(1)));
 	}
 
 	@Test
 	void test_fork_has_parent_clients() {
-		BreakbotConfig.Clients config = new BreakbotConfig.Clients(10, 0, Collections.emptyList());
+		BreakbotConfig.Clients config = new BreakbotConfig.Clients(5, 1, Collections.emptyList());
 		List<BreakbotConfig.GitHubRepository> clients = clientsService.buildClientsList(
-			new Repository("break-bot", "spoon-fork-for-tests", "", ""),
+			new Repository("break-bot", "drill-fork-for-tests", "", ""),
 			config,
-			"fr.inria.gforge.spoon:spoon-core");
-		assertThat(clients, hasSize(10));
+			"org.apache.drill.exec:drill-rpc");
+		assertThat(clients, hasSize(5));
 	}
 }
