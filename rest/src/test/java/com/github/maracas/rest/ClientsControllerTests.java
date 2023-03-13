1a8aef1e2ef4d18a7b987f09bfc104815e7ff8a2
Thomas Degueule <thomas.degueule@labri.fr>
2023-03-13 12:05:42 +0100

    rest: ClientsControllerTests isn't an AbstractControllerTest

--- rest/src/test/java/com/github/maracas/rest/ClientsControllerTests.java
+++ rest/src/test/java/com/github/maracas/rest/ClientsControllerTests.java
@@ -1,13 +1,24 @@
 package com.github.maracas.rest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class ClientsControllerTests extends AbstractControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class ClientsControllerTests {
+	@Autowired
+	protected MockMvc mvc;
+
 	@Test
 	void get_clients_spoon() throws Exception {
 		mvc.perform(get("/github/clients/INRIA/spoon"))
